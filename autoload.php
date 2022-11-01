e792d525f7a3a437c5756815b27627dc9fbed23d
omartins-zs <gabrielmatheus209@gmail.com>
2022-11-01 18:01:01 -0300

    Criando autoload dos controllers e scan da pasta

---
+++ autoload.php
@@ -0,0 +1,36 @@
+<?php
+
+// Iniciando session
+session_start();
+
+// Data padrão Brasil
+date_default_timezone_set('America/Sao_Paulo');
+
+class Autoload
+{
+    public function __construct()
+    {
+        spl_autoload_extensions('.class.php');
+        spl_autoload_register(array($this, 'load'));
+    }
+
+    private function load($className)
+    {
+        $extensions = spl_autoload_extensions();
+        require_once 'class/' . $className . $extensions;
+    }
+}
+$autoload = new Autoload();
+
+$s = scandir('class');
+$arraClass = array();
+
+foreach ($s as $k) {
+    if ($k != "." && $k != "..") {
+        $ex = explode('.', $k);
+        $class = $ex[0];
+        $arrayClass[$class] = new $class();
+    }
+}
+
+$i = (object)$arrayClass;
